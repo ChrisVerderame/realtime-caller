9d54a9ecdb1833a71f324ebf1fe090b09723cf87
ChrisVerderame <chris@blacklineacq.com>
2026-04-23 19:53:59 -0400

    Update dialer.js

--- dialer.js
+++ dialer.js
@@ -1,19 +1,15 @@
-const twilio = require("twilio");
-
-const client = twilio(
+const client = require("twilio")(
   process.env.TWILIO_SID,
   process.env.TWILIO_AUTH
 );
 
 async function callLead(lead) {
-  console.log("CALLING:", lead.phone);
-
   return client.calls.create({
     to: lead.phone,
     from: process.env.TWILIO_NUMBER,
-    url: `${process.env.BASE_URL}/twilio-voice?name=${encodeURIComponent(
-      lead.name
-    )}&address=${encodeURIComponent(lead.address)}`
+
+    // 🚨 THIS IS THE FIX
+    url: `sip:${process.env.LIVEKIT_SIP_ENDPOINT}`
   });
 }
 
